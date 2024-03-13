bcd4c85fbf4637d3e9ced1b33ff6519b002f96f8
MateuszBieda <mateusbieda@gmail.com>
2024-03-13 19:55:34 +0100

    index.js update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import { persistor, store } from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
@@ -10,8 +10,8 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate>
-        <BrowserRouter basename="/goit-react-hw-08-phonebook">
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="/goit-react-hw-08-phonebook/">
           <App />
         </BrowserRouter>
       </PersistGate>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>Phone Book welcome page</h1>
+      <h1>Phonebook welcome page</h1>
     </div>
   );
 }
