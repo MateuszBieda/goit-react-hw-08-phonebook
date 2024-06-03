f2eef85f6dab8443f8d4e30ffd72f3fd5148483c
MateuszBieda <mateusbieda@gmail.com>
2024-06-03 17:37:55 +0200

    adding css modules

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -1,5 +1,6 @@
 import { useDispatch } from 'react-redux';
 import { register } from '../../redux/auth/operations';
+import css from './RegisterForm.module.css';
 
 export const RegisterForm = () => {
   const dispatch = useDispatch();
@@ -18,16 +19,16 @@ export const RegisterForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} autoComplete="off">
-      <label>
+    <form onSubmit={handleSubmit} autoComplete="off" className={css.form}>
+      <label className={css.label}>
         Username
         <input type="text" name="name" />
       </label>
-      <label>
+      <label className={css.label}>
         Email
         <input type="email" name="email" />
       </label>
-      <label>
+      <label className={css.label}>
         Password
         <input type="password" name="password" />
       </label>
